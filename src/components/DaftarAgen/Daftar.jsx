3a5f52cb70f12b0cbfc9baec1fe69e3da55fc5d8
ikhsanasagaf <ikhsan.asagaf2012@gmail.com>
2025-11-23 17:55:43 +0700

    Final web

--- src/components/DaftarAgen/Daftar.jsx
+++ src/components/DaftarAgen/Daftar.jsx
@@ -2,8 +2,30 @@ import React from "react";
 
 const Daftar = () => {
   return (
-    <div className="container justify-center ml-35 mt-20">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScXwrrJNz7vSux9uPOlTzF_2mPTJG8JKwCCF7tCTiTyUKmFXw/viewform?embedded=true" width="1200" height="2000" frameborder="0" marginheight="0" marginwidth="0">Memuat…</iframe>
+    <div className="py-16">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-bold text-cyan-900">
+          Pendaftaran Agen
+        </h1>
+        <p className="text-gray-600 mt-4">
+          Silakan isi formulir di bawah ini untuk mendaftar sebagai agen resmi
+          Moedal Air Mineral.
+        </p>
+      </div>
+
+      <div className="w-full max-w-3xl mx-auto">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScXwrrJNz7vSux9uPOlTzF_2mPTJG8JKwCCF7tCTiTyUKmFXw/viewform?embedded=true"
+          width="100%" // Diubah ke 100%
+          height="2180" // Tinggi tetap sesuai embed code Anda
+          frameBorder="0" // Atribut HTML diubah ke JSX (camelCase)
+          marginHeight="0"
+          marginWidth="0"
+          className="w-full" // Menambahkan kelas w-full untuk memastikan lebar 100%
+        >
+          Memuat…
+        </iframe>
+      </div>
     </div>
   );
 };
